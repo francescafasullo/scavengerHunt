045cfc13278fc4e8b5767f26861cf0d975c8e4ca
Francesca Fasullo <fjf2107@columbia.edu>
2017-06-26 12:54:25 -0400

    setting up geolocation

--- scavengerHunt/src/Components/Home.js
+++ scavengerHunt/src/Components/Home.js
@@ -25,6 +25,29 @@ console.log('page loaded')
 export default class HomeScreen extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null
+    }
+  }
+
+  componentDidMount() {
+    this.watchId = navigator.geolocation.watchPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        })
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 2000, maximumAge: 1000, distanceFilter: 10 }
+    )
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId)
   }
 
   render() {
@@ -34,6 +57,11 @@ export default class HomeScreen extends Component {
         <Text style={styles.welcome}>
           Welcome to Scavenger Hunt!
         </Text>
+        <Text>
+          Latitude: {this.state.latitude}
+          Longitude: {this.state.longitude}
+        </Text>
+        {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
         <Button onPress={() => {this.props.navigation.navigate('Camera')}} title="Play!" />
       </View>
     );
