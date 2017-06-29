aa5aa9443fb9c02c3493d50a9256022b246307de
Nitika Nadgar <nitiquita@gmail.com>
2017-06-29 16:31:10 -0400

    merge conflicts resolved

---
+++ scavengerHunt/src/reducers/mapsReducer.js
@@ -0,0 +1,54 @@
+import store from '../../store'
+const fireBaseFunctions = require('../../database/firebase');
+import {writeUserData} from '../../database/firebase'
+import firebase from 'firebase'
+
+/* ------------------ actions ------------------------ */
+const ADD_MAP = 'ADD_MAP'
+
+/* ------------------ action creators ---------------- */
+export const setLoggedUser = (userId) => ({ type: SET_LOGGED_USER, userId})
+
+/* ------------------ reducer ------------------------ */
+const initialAuthState = {
+	userId: undefined
+}
+
+const authReducer = (state = initialAuthState, action) => {
+	switch (action.type) {
+		case SET_LOGGED_USER:
+			return Object.assign({},state, {userId: action.userId});
+		default:
+			return initialAuthState;
+
+	}
+
+}
+
+
+
+/* ------------------ dispatchers ------------------- */
+
+export const signUp = (username, email, password) => dispatch => 
+	firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(res => 
+                writeUserData(res.uid, username, email, 500, "url"));
+
+
+
+export const login = (email,password) => dispatch => 
+	firebase.auth().signInWithEmailAndPassword(email, password);
+
+
+export const logout = () => dispatch => 
+	firebase.auth().signOut();
+	
+
+
+  
+
+
+export default authReducer
+
+
+
