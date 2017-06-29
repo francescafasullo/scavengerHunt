6b7acdba75d7084df3feb7ea8039a436b833dccf
Nitika Nadgar <nitiquita@gmail.com>
2017-06-28 21:32:49 -0400

    empty components to be filled

---
+++ scavengerHunt/src/Components/EditSH.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { AppRegistry, StyleSheet, Text, View, Button, Image } from 'react-native'
+
+export default class EditSH extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>
+                    {/*Scavenger Hunt Name*/}
+                    {/*MAP*/}
+                    {/*List locations that can be deleted*/}
+                    {/*Select and add token*/}
+                </Text>
+            </View>
+        )
+    }
+}
+
+AppRegistry.registerComponent('scavengerHunt', () => EditSHScreen)
