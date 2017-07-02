9cfd57044ebfadb857049a4f181095d360b42ed0
Nitika Nadgar <nitiquita@gmail.com>
2017-07-02 15:22:46 -0400

    fixed formatting

--- scavengerHunt/src/reducers/authReducer.js
+++ scavengerHunt/src/reducers/authReducer.js
@@ -1,13 +1,13 @@
 import store from '../../store'
 const fireBaseFunctions = require('../../database/firebase');
-import {writeUserData} from '../../database/firebase'
+import { writeUserData } from '../../database/firebase'
 import firebase from 'firebase'
 
 /* ------------------ actions ------------------------ */
 const SET_LOGGED_USER = 'SET_LOGGED_USER'
 
 /* ------------------ action creators ---------------- */
-export const setLoggedUser = (userId) => ({ type: SET_LOGGED_USER, userId})
+export const setLoggedUser = (userId) => ({ type: SET_LOGGED_USER, userId })
 
 /* ------------------ reducer ------------------------ */
 const initialAuthState = {
@@ -17,7 +17,7 @@ const initialAuthState = {
 const authReducer = (state = initialAuthState, action) => {
 	switch (action.type) {
 		case SET_LOGGED_USER:
-			return Object.assign({},state, {userId: action.userId});
+			return Object.assign({}, state, { userId: action.userId });
 		default:
 			return state;
 
@@ -29,33 +29,33 @@ const authReducer = (state = initialAuthState, action) => {
 
 /* ------------------ dispatchers ------------------- */
 
-export const signUp = (username, email, password) => dispatch => 
+export const signUp = (username, email, password) => dispatch =>
 	firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then(res => 
-                writeUserData(res.uid, username, email, 500, "url")).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});;
+		.then(res =>
+			writeUserData(res.uid, username, email, 500, "url")).catch(function (error) {
+				// Handle Errors here.
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				// ...
+			});;
 
 
 
-export const login = (email,password) => dispatch => 
-	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});;
+export const login = (email, password) => dispatch =>
+	firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		// ...
+	});;
 
 
-export const logout = () => dispatch => 
+export const logout = () => dispatch =>
 	firebase.auth().signOut();
-	
 
 
-  
+
+
 
 
 export default authReducer
