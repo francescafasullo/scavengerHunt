bcb0c115ec9162481c99bd219ae87f4e63ced25e
Jasmine Zangi <jasmine.zangi@gmail.com>
2017-07-06 12:35:45 -0400

    Styling app #73 (#80)
    * added styling to my account and camera
    
    * styled my account
    
    * styled components

--- scavengerHunt/src/Components/SignInSignUp.js
+++ scavengerHunt/src/Components/SignInSignUp.js
@@ -4,9 +4,8 @@ import {
   StyleSheet,
   Text,
   View,
-  Navigator,
-  Button
-} from 'react-native';
+  Navigator} from 'react-native';
+import {Button} from 'react-native-elements'
 import styles from '../../stylesheet'
 
 export default class SignInSignUp extends Component {
@@ -17,12 +16,12 @@ export default class SignInSignUp extends Component {
 
 	render(){
 		return (
-		  <View style={styles.container}>
-	        <Text style={styles.welcome}>
+		  <View style={styles.myAccount_container}>
+	        <Text style={styles.myAccount_title}>
 	          Sign In or Sign Up
 	        </Text>
-	        <Button onPress={() => { this.props.navigation.navigate('SignUp') }} title="Sign Up" />
-	        <Button onPress={() => {this.props.navigation.navigate('Login')}}
+	        <Button buttonStyle={styles.myAccount_button} onPress={() => { this.props.navigation.navigate('SignUp') }} title="Sign Up" />
+	        <Button buttonStyle={styles.myAccount_button} onPress={() => {this.props.navigation.navigate('Login')}}
 	        title="Login"/>
 		 </View>
 	);
